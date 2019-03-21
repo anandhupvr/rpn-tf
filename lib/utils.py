90e44d170b124157b51fea69a5a803a8cb09618d
anandhupvr <anandhupvr@gmail.com>
2019-03-21 16:50:36 +0000

    minor

--- lib/utils.py
+++ lib/utils.py
@@ -303,6 +303,7 @@ def rpn_to_roi(rpn_layer, regr_layer, C, dim_ordering, use_regr=True, max_boxes=
 
 
 def bbox_plot(img, box):
+	import pdb; pdb.set_trace()
     img = img.resize([224, 224])
     im = (np.array(img, dtype=np.uint8))
     fig, ax = plt.subplots(1)
@@ -313,4 +314,4 @@ def bbox_plot(img, box):
         s = patches.Rectangle((box[i][k], box[i][k+1]), box[i][k+2], box[i][k+3], linewidth=1, edgecolor='g', facecolor="none")
         # s = patches.Rectangle((box[i][0], box[i][1]), box[i][2], box[i][3], linewidth=1, edgecolor='g', facecolor="none")
         ax.add_patch(s)
-    im.savefig("prediction.png", im)
+    plt.savefig("prediction.png", im)
