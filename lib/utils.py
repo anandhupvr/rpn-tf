9bdab50c9a556fa877cc06a534d87a0e2c366f46
anandhupvr <anandhupvr@gmail.com>
2019-03-21 12:05:28 +0000

    infer

--- lib/utils.py
+++ lib/utils.py
@@ -301,7 +301,7 @@ def rpn_to_roi(rpn_layer, regr_layer, C, dim_ordering, use_regr=True, max_boxes=
 
 def bbox_plot(img, box):
     img = img.resize([224, 224])
-    im = np.reshape(np.array(img, dtype=np.uint8))
+    im = (np.array(img, dtype=np.uint8))
     fig, ax = plt.subplots(1)
     # import pdb; pdb.set_trace()
     ax.imshow(im)
