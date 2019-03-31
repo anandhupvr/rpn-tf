44a96792202e8ca22e6a0a575945fe5411ea4698
anandhupvr <anandhupvr@gmail.com>
2019-03-31 06:11:04 +0000

    inference

--- prediction.py
+++ prediction.py
@@ -43,10 +43,10 @@ with tf.Session(graph=new_graph) as sess:
     image_tensor = tf.get_default_graph().get_tensor_by_name('input_image:0')
     rpn_cls = tf.get_default_graph().get_tensor_by_name('rpn_cls_reshaped:0')
     rpn_box = tf.get_default_graph().get_tensor_by_name('rpn_bbox_reshaped:0')
-
+    import pdb; pdb.set_trace()
     rpn = sess.run([rpn_cls, rpn_box], feed_dict={image_tensor:img})
     boxes = rpn[1].reshape(rpn[1].shape[0], rpn[1].shape[1]*rpn[1].shape[2], rpn[1].shape[3])
     boxes[0][:,[0, 2]] = boxes[0][:,[0, 2]] * x_ratio
     boxes[0][:, [1, 3]] = boxes[0][:, [1, 3]] * y_ratio
-    nms_box = utils.non_max_suppression_fast(np.abs(boxes[0]), 0.5)
+    nms_box = utils.non_max_suppression_fast(np.abs(boxes[0]), 0.4)
     utils.bbox_plot(img_, nms_box)
